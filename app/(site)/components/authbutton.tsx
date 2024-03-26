ce74997286d82225692eca2c27bde05a225495a0
kartiknayak1216 <nayakkartik1216@gmail.com>
2024-03-26 16:31:44 +0530

    authentication

---
+++ app/(site)/components/authbutton.tsx
@@ -0,0 +1,66 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { BsGithub, BsGoogle  } from 'react-icons/bs';
+import {signIn,useSession} from 'next-auth/react'
+import { Toaster, toast } from 'sonner'
+import { LuLoader2 } from "react-icons/lu";
+import { useRouter } from 'next/navigation';
+export default function Authbutton() {
+  
+    const [isLoading, setIsLoading] = useState(false);
+ 
+    const session = useSession()
+    const router = useRouter();
+
+
+
+    useEffect(() => {
+      if (session?.status === 'authenticated') {
+        toast.success("Acoount Verified")
+        router.push('/conversations')
+      }
+    }, [session?.status, router]);
+
+
+    const socialAction = (action: string) => {
+        setIsLoading(true);
+
+        signIn(action, { redirect: false })
+          .then((callback) => {
+            if (callback?.error) {
+              toast.error('Invalid credentials!');
+            }
+    
+            if (callback?.ok) {
+              toast.success("Acoount Verified")
+              router.push("/conversations")
+            }
+          })
+          .finally(() => setIsLoading(false));
+          ;
+      } 
+    
+    
+      return (
+       <div className="flex min-h-screen flex-col justify-center items-center
+       py-12 sm:px-6 lg:px-8">
+        <div className="mx-auto ">
+    <Image src="/logo.png" alt={"logo"} width="48" height="48" />
+        </div>
+    <div className=" mt-6 text-center text-3xl font-bold text-secondary-foreground">
+      Proceed to your account
+    </div>
+    <Button variant={"ghost"}className="flex gap-4  text-center mt-12 md:mt-9  text-muted-foreground "
+    onClick={() => socialAction('google')}
+    disabled={isLoading}>
+      <BsGoogle  className="text-xl"/>
+     <div className="text-center text-xl font-semibold">
+        Continue with Google
+      
+  </div>
+    </Button >
+       </div>
+      );
+    }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,21 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster, toast } from 'sonner'
+import AuthContext from "./context/Authcontext";
 
+
+
+function App() {
+  return (
+    <div>
+      <Toaster richColors />
+      <button onClick={() => toast('My first toast')}>
+        Give me a toast
+      </button>
+    </div>
+  )
+}
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <AuthContext>
+        <Toaster/>{children}   </AuthContext></body>
     </html>
   );
 }
